4965000803af45d1fb527d3fd41e6101fed80609
anthonyOliv94 <anthony.oliv94@gmail.com>
2025-09-22 12:03:33 -0300

    update
    continuing ...

--- tictactoe.py
+++ tictactoe.py
@@ -9,11 +9,12 @@ winner = None
 
 # PRINT BOARD
 def printBoard(board):
-    print("\n".join(" | ".join(linhas) for linhas in board))
+    print("\n" + "\n".join(" | ".join(linhas) for linhas in board) + "\n")
 
 # SWITCH PLAYER
-def switchPlayer(currentPlayer):
-    if currentPlayer.upper() == "X":
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == "X":
         currentPlayer = "O"
     else:
         currentPlayer = "X"
@@ -30,5 +31,45 @@ def playerInput(board):
         coluna = (inp-1)%3
 
     board[linha][coluna] = currentPlayer
+    printBoard(board)
+
+# CHECK WINNER
+def checkRows(board):
+    global winner
+    for i in range(len(board)):
+        if len(set(board[i])) == 1 and "-" not in set(board[i]):
+            winner = board[i][0]
+            return True
+        
+def checkColumns(board):
+    global winner        
+    for i in range(len(board)):
+        if len(set(board[:][i])) == 1 and "-" not in set(board[:][i]):
+            winner = board[0][i]
+            return True
+
+def checkDiag(board):
+    global winner
+    if len(set(board[n][n] for n in range(len(board)))) == 1 and board[0][0] != "-":
+        winner = board[0][0]
+        return True
+    elif len(set(board[i][len(board) - 1 - i] for i in range(len(board)))) == 1 and board[0][2] != "-":
+        winner = board[0][2]
+        return True
+    
+# CHECK RESULT
+def checkResult():
+    global gameRunning
+    if checkRows(board) or checkColumns(board) or checkDiag(board):
+        gameRunning = False
+        print(f"O jogador '{winner}' ganhou.")
+    elif not any("-" in board[i] for i in range(len(board))):
+        gameRunning = False
+        print("Empate")
 
 printBoard(board)
+
+while gameRunning:
+    playerInput(board)
+    switchPlayer()
+    checkResult()
